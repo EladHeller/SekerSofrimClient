e8cbc28749f6e5aa2e1f4346b47ab649cc10196a
eladheller <eladheller@gmail.com>
2017-03-20 09:40:37 +0200

    lastLetter

--- js/xl-json.js
+++ js/xl-json.js
@@ -40,7 +40,7 @@ function workbookToArrays(workbook){
     var data = [];
     var currLetter = 'A';
     var currIndex = 1;
-    var lastLetter = 'H'    
+    var lastLetter = 'H';
     while (sheet[currLetter + currIndex]) {
         data[currIndex-1] = data[currIndex-1] || [];
         var currCell = sheet[currLetter + currIndex];
