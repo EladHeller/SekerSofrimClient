145a1a5ad355f922194323e01d7934cfb52422ef
eladheller <eladheller@gmail.com>
2017-06-25 22:10:23 +0300

    fix image loader warnings

--- webpack.config.js
+++ webpack.config.js
@@ -25,19 +25,22 @@ module.exports = {
             },{
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 loaders: ['file-loader?context=src/images&name=images/[path][name].[ext]', {
-                loader: 'image-webpack-loader',
-                query: {
-                    progressive: true,
-                    optimizationLevel: 4,
-                    interlaced: false,
-                    optipng: {
-                    optimizationLevel: 4,
-                    },
-                    pngquant: {
-                    quality: '75-90',
-                    speed: 3,
-                    },
-                },
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        optipng: {
+                            optimizationLevel: 4,
+                        },
+                        pngquant: {
+                            quality: '75-90',
+                            speed: 3,
+                        }
+                    }
                 }],
                 exclude: /node_modules/,
                 include: __dirname,
