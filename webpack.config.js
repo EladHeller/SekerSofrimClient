4d8ef313af1e2008be49c54168879a93ea907c20
Elad Heller <eladheller@gmail.com>
2017-11-29 08:42:16 +0200

    style, index.html location

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ const webpackConfig = {
         ]
     },
     plugins : [
-        new HtmlWebpackPlugin({hash: true,template:'./index.html' })
+        new HtmlWebpackPlugin({hash: true,template:'./src/index.html' })
     ]
 };
 
